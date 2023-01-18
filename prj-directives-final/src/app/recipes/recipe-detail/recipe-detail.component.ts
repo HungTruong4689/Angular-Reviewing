af0f73cb2bbcbb164163a006243300cb946e7a8c
HungTruong4689 <hungphuc4689@gmail.com>
2023-01-18 07:09:21 +0200

    routing

--- prj-directives-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-directives-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipesService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipesService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
-
 }
--- prj-directives-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ prj-directives-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
+import { RecipesService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  @Output() recipeSelected = new EventEmitter<void>();
+  //@Output() recipeSelected = new EventEmitter<void>();
 
-  constructor() { }
+  constructor(private recipeService: RecipesService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSelected() {
-    this.recipeSelected.emit();
+    //this.recipeSelected.emit();
+    this.recipeService.recipeSelected.emit(this.recipe);
   }
-
 }
