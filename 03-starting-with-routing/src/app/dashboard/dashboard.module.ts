eff1d772ebead00eded64f83f4d76c93f1d23c91
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-29 17:42:38 +0300

    declaration on module

--- 03-starting-with-routing/src/app/dashboard/dashboard.module.ts
+++ 03-starting-with-routing/src/app/dashboard/dashboard.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
+  //declaration 
   declarations: [DashboardComponent],
   imports: [DashboardRoutingModule]
 })
