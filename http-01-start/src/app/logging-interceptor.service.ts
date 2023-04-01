98141a6fdb5915e8c4b6e660ba0cba74ee6798bf
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-01 18:06:44 +0300

    intercept value

--- http-01-start/src/app/logging-interceptor.service.ts
+++ http-01-start/src/app/logging-interceptor.service.ts
@@ -6,7 +6,11 @@ import {
 } from "@angular/common/http";
 import { tap } from "rxjs/operators";
 
+
+
 export class LoggingInterceptorService implements HttpInterceptor {
+
+  //intercept value
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log("Outgoing request");
     console.log(req.url);
