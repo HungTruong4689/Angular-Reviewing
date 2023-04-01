c65a935729cf97e9003e62fea531af29a3b410a6
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-01 18:05:13 +0300

    req.clone

--- http-01-start/src/app/auth-interceptor.service.ts
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -11,6 +11,8 @@ export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     //console.log("Request is on its way");
     //console.log(req.url);
+
+    //req.clone
     const modifiedRequest = req.clone({
       headers: req.headers.append("Auth", "xyz"),
     });
