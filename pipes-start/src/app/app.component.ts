7e5bf1eae1916448962243a4c062ace5f1b7b95d
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-07 21:19:25 +0300

    review promise

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  //review promise
   appStatus = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve('stable');
