965d9751674aa4547a64d131849ffabe85591e45
HungTruong4689 <hungphuc4689@gmail.com>
2023-04-07 21:17:08 +0300

    set up pipe

--- pipes-start/src/app/shorten.pipe.ts
+++ pipes-start/src/app/shorten.pipe.ts
@@ -1,9 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+//set up pipe
 @Pipe({
   name: 'shorten',
 })
 export class ShortenPipe implements PipeTransform {
+  
   transform(value: any, limit: number) {
     if (value.length >= limit) {
       return value.substr(0, limit) + '...';
