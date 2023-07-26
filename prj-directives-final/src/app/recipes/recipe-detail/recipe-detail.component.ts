b96182a2f73d233635c32beb57cf9df698273f69
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-26 21:12:47 +0300

    route navigate

--- prj-directives-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ prj-directives-final/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -36,6 +36,7 @@ export class RecipeDetailComponent implements OnInit {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
   onEditRecipe() {
+    //navigate route
     this.router.navigate(['edit'], { relativeTo: this.route });
     // this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route });
   }
