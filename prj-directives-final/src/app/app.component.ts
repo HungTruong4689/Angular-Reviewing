7df3d648b89d9c78438400df66ec4b52eb335cb8
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-26 20:56:47 +0300

    send data

--- prj-directives-final/src/app/app.component.ts
+++ prj-directives-final/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   loadedFeature = 'recipe';
 
+  //how to send this variable
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
