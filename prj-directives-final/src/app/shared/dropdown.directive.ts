20698f80b5cde03a772a8762b3ce81d9d8434cb9
HungTruong4689 <hungphuc4689@gmail.com>
2023-07-30 22:09:44 +0300

    toggleOpen

--- prj-directives-final/src/app/shared/dropdown.directive.ts
+++ prj-directives-final/src/app/shared/dropdown.directive.ts
@@ -7,6 +7,7 @@ export class DropdownDirective {
   //hostbinding
   @HostBinding('class.open') isOpen = false;
 
+  //host listener
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
