1789401059f2824efd77bbd451ab031986a54d21
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-09 09:58:12 +0300

    loading server

--- routing-start/src/app/home/home.component.ts
+++ routing-start/src/app/home/home.component.ts
@@ -12,6 +12,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {}
 
+  //Loading server
   onLoadServers(id: number) {
     // complex calculation
     this.router.navigate(['/servers', id, 'edit'], {
