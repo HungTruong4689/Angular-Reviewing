a7117897e44168e924f64b69e3a18aed339007b2
HungTruong4689 <hungphuc4689@gmail.com>
2023-08-27 20:40:06 +0300

    directive

--- prj-http-01-save-and-fetch-recipes/src/app/shared/dropdown.directive.ts
+++ prj-http-01-save-and-fetch-recipes/src/app/shared/dropdown.directive.ts
@@ -1,12 +1,13 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding } from "@angular/core";
 
+//What is the directive
 @Directive({
-  selector: '[appDropdown]'
+  selector: "[appDropdown]",
 })
 export class DropdownDirective {
-  @HostBinding('class.open') isOpen = false;
+  @HostBinding("class.open") isOpen = false;
 
-  @HostListener('click') toggleOpen() {
+  @HostListener("click") toggleOpen() {
     this.isOpen = !this.isOpen;
   }
 }
