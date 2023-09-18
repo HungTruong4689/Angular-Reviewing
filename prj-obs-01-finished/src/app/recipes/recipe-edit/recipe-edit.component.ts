bd0df52161f7f1c9b52e7b9aeb9815864b0109cb
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-18 21:48:58 +0300

    formControl

--- prj-obs-01-finished/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ prj-obs-01-finished/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -61,7 +61,7 @@ export class RecipeEditComponent implements OnInit {
     //FormArray recipeForm
     (<FormArray>this.recipeForm.get('ingredients')).push(
       new FormGroup({
-        name: new FormControl(null, Validators.required),
+        name: new FormControl(null, Validators.required), //formControl
         amount: new FormControl(null, [
           Validators.required,
           Validators.pattern(/^[1-9]+[0-9]*$/),
