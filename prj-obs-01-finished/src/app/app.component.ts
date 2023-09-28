12c663d03da11dadf63264be555b3b7fdfa2362a
HungTruong4689 <hungphuc4689@gmail.com>
2023-09-28 20:18:48 +0300

    selector

--- prj-obs-01-finished/src/app/app.component.ts
+++ prj-obs-01-finished/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //selector
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-}
+export class AppComponent {}
