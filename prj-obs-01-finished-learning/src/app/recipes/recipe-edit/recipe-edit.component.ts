2deb28f1c1493d15d16f21abee5c49a2971d1b9e
HungTruong4689 <hungphuc4689@gmail.com>
2023-10-02 19:19:19 +0300

    get recipe

--- prj-obs-01-finished-learning/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ prj-obs-01-finished-learning/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -52,7 +52,7 @@ export class RecipeEditComponent implements OnInit {
     let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
-      const recipe = this.recipeService.getRecipe(this.id);
+      const recipe = this.recipeService.getRecipe(this.id); //get Recipe
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
